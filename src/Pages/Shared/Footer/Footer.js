05a1471ed94ae51f623f9389cb6aa4336a05a4a1
jibananando <jibananando.pramanik@gmail.com>
2022-11-24 03:09:23 +0600

    simple installation complate

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h3><small>Footer comming</small></h3>
+        </div>
+    );
+};
+
+export default Footer;
