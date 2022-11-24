5628c5eb9add61861ae64e183088e14c3f6a3cb4
jibananando <jibananando.pramanik@gmail.com>
2022-11-24 14:39:26 +0600

    created a footer

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             <li><Link to="/login">Login</Link></li>
         </React.Fragment>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-orange-600 text-white">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
